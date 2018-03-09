c2eb0769fd886b05d6c64d67e630b8aaede01786
srinivas.kolluri <srinivas.kolluri@pwc.com>
2018-03-09 11:44:44 -0600

    Testing Git

--- SKCore2App1/Program.cs
+++ SKCore2App1/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
+// TEST Comment
 namespace SKCore2App1
 {
     public class Program
